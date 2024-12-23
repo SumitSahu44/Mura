cd734ef57c660be7080315159ebb01f72bbdbd06
SumitSahu444 <sumitkumarsahu141@gmail.com>
2024-12-23 14:13:02 +0530

    nav done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import {Route, Routes} from 'react-router-dom';
 import Home from './pages/Home/Home';
+import Navbar from './components/Navbar/Navbar';
 const App = () => {
   return (
     <div>
-      <h1>hii</h1>
-
-
-        
-  <Routes>
-  <Route path='/' element={<Home/>} />
-  </Routes>
+      <Navbar/>
+      <Routes>
+         <Route path='/' element={<Home/>} />
+      </Routes>
     </div>
 
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
-
+import './Navbar.css'
 const Navbar = () => {
   return (
-    <div>
-      navbar
+    <div className='nav-container'>
+        <nav className='nav'>
+            <div className='logo'>
+                MURA
+            </div>
+            <div className='left-nav'>
+                    <div>
+                        <ul>
+                            <li>Home</li>
+                            <li>About Us</li>
+                            <li>Process</li>
+                            <li>FAQ</li>
+                        </ul>
+                    </div>
+                    <div>
+                       <button className='btn'>Try Now</button>
+                   </div>
+            </div>
+           
+             
+        </nav>
     </div>
   )
 }
