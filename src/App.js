769645f356b247fa8f3c991d1d930269db20ab2a
SumitSahu444 <sumitkumarsahu141@gmail.com>
2024-12-23 13:51:27 +0530

    react home page structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react'
-
+import {Route, Routes} from 'react-router-dom';
+import Home from './pages/Home/Home';
 const App = () => {
   return (
     <div>
       <h1>hii</h1>
+
+
+        
+  <Routes>
+  <Route path='/' element={<Home/>} />
+  </Routes>
     </div>
+
+
+
+
+
+
   )
 }
 
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Home.css'
+
+import Navbar from '../../components/Navbar/Navbar'
+import Header from '../../components/Header/Header'
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Header/>
+    </div>
+  )
+}
+
+export default Home
