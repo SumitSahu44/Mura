46753cd14a8c84080e7444e02e645c89e2ada464
Sumit Sahu <99080079+SumitSahu44@users.noreply.github.com>
2024-12-28 23:48:50 +0530

    Update Model.jsx

--- src/components/Model/Model.jsx
+++ src/components/Model/Model.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useGLTF } from "@react-three/drei";
 
+
+// 3d skeleton model code 
 const Model = () => {
   const modelUrl = "/images/tripo_pbr_model_585d9e38-94f0-415b-aa0d-6b74503a517a (1).glb"; // Path to your 3D model
   const { scene } = useGLTF(modelUrl); // Load the 3D model
